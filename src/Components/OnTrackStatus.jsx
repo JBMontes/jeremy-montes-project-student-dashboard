ab4501482149bd5e4c80b43d8bfb2d2669d33e95
Jeremy Montes <jeremymontes@pursuit.org>
2023-08-03 14:55:11 -0400

    Made edits to Header.css

--- src/Components/OnTrackStatus.jsx
+++ src/Components/OnTrackStatus.jsx
@@ -1,15 +1,19 @@
-function OnTrackStatus({data, students}){
+function OnTrackStatus({ data, students }) {
 
-    { if((students.certifications.github && students.certifications.linkedin && students.certifications.mockInterview && students.certifications.resume === true) && (students.codewars.current.total > 600)){
-          return (
-            <h3><span> On-Track</span></h3>
-          )
-          }else{
-            return(
-              <h3><span> Off-Track</span></h3>
+    {
+        if ((students.certifications.github &&
+            students.certifications.linkedin &&
+            students.certifications.mockInterview &&
+             students.certifications.resume === true) &&
+            (students.codewars.current.total > 600)) {
+            return (
+                <h3><span> On-Track</span></h3>
             )
-        } 
-     }
-
+        } else {
+            return (
+                <h3><span> Off-Track</span></h3>
+            )
+        }
+    }
 }
 export default OnTrackStatus
