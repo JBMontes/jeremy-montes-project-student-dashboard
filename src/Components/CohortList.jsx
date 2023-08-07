6ee6959f3cfda9eed6021345dc47172e490b82ef
Jeremy Montes <jeremymontes@pursuit.org>
2023-08-07 09:12:22 -0400

    Made some css edits

--- src/Components/CohortList.jsx
+++ src/Components/CohortList.jsx
@@ -1,71 +1,74 @@
 import data from "../data/data.json";
 import { useState } from "react";
 import "../Components/CohortList.css";
-import StudentList from "./StudentList.jsx";
 import StudentCard from "./StudentCard.jsx";
 
 
 function CohortList() {
-    
+
+    //Holds Student Cohort
+    const [cohortYear, setCohortYear] = useState("All Students")
 
     // Holds the all data
     const [selectedCohort, setSelectedCohort] = useState(data);
 
-    
+    let totalAllStudents = selectedCohort.length
+
+    //Holds Amount of student
+    const [studentAmount, setStudentAmount] = useState(totalAllStudents)
+
     // Holds cohort semester
     const [season, setSeason] = useState(selectedCohort);
 
     // creates array of filtered cohort
     let filter = selectedCohort.filter(
-        (semester) => semester.cohort.cohortCode === season).map((students)=>{
+        (semester) => semester.cohort.cohortCode === season).map((students) => {
 
-            return(
+            return (
                 <>
-                <StudentCard students={students} />
+                    <StudentCard students={students} />
                 </>
             )
         })
-        
-    //Holds filtered Cohort
-    const [filteredCohort, setFilteredCohort] = useState(filter);
- 
-    let [showComponent, setShowComponent] = useState([])
 
-    let RenderAllStudents = selectedCohort.map((students)=>{
+    let RenderAllStudents = selectedCohort.map((students) => { return (<><StudentCard students={students} /></>) })
 
-            return(
-                <>
-             <StudentCard students={students} />
-                </>
-            )
-        })
-    
-    function AllSeason() {
+    //Holds Component in div tag
+    let [showComponent, setShowComponent] = useState(RenderAllStudents)
 
+
+    //Shows All Students
+    function AllSeason(e) {
+
+        setCohortYear(e.target.innerText)
         setShowComponent(RenderAllStudents)
+        setStudentAmount(totalAllStudents)
 
     }
+    //Shows Students depending on their cohort year
     function ChangeSeason(e) {
         let space = e.target.innerText.replace(" ", "")
 
+        setCohortYear(e.target.innerText)
+
         setSeason(space)
 
-      setShowComponent(filter)
-     
-        console.log(filter)
-        
+        setShowComponent(filter)
+
+        setStudentAmount(filter.length)
+
     }
 
     return (
         <>
             <div className="cohortList">
-                
+
                 <ul>
                     Choose a Class By Start Date
                     <li
-                        onClick={() => {
+                        onClick={(e) => {
 
-                            AllSeason();
+                            AllSeason(e);
                         }}
                     >
                         All Students
@@ -122,15 +125,19 @@ function CohortList() {
                     <li
                         onClick={(e) => {
                             ChangeSeason(e);
-                        }}>Spring 2025
+                        }}>
+                        Spring 2025
                     </li>
                 </ul>
+
+                <div className="studentCard">
+
+                    <div className="studentCount"> {cohortYear} <br />Total Students: {studentAmount} </div>
+
+                    {showComponent}
+                </div>
             </div>
 
-            <div className="studentCard">
-                {showComponent}
-
-            </div>
         </>
     );
 }
@@ -144,138 +151,3 @@ export default CohortList;
 
 
 
-
-// import data from "../data/data.json"
-// // import StudentList from "./StudentList.jsx"
-// import '../Components/CohortList.css'
-// import OnTrackStatus from "./OnTrackStatus.jsx"
-// import { useState } from "react"
-// // import FilteredSeasonCard from "./FilteredSeasonCard.jsx"
-
-// function CohortList(){
-
-//  const [ selectedCohort,setSelectedCohort ] = useState(data);
-//  let [ season,setSeason] = useState("Winter2025")
-
-//  //   console.log(selectedCohort.filter((semester)=>semester.cohort.cohortCode === season))
-
-// //  function FilteredSeasonCard(){
-
-// //      const [changeButtonText, setChangeButtonText] = useState("Show More")
-// //      const [isButtonPressed, setIsButtonPressed] = useState(true)
-// //      const [extraInfo, setExtraInfo] = useState(null)
-
-// //     selectedCohort.filter((semester)=> semester.cohort.cohortCode === season).map((students) => {
-
-// //         let percentage = Math.trunc((students.codewars.current.total / students.codewars.goal.total) * 100)
-
-// //         function symbolForCert(cert) {
-// //             if (cert === false) {
-// //                 return (<li> ❌ </li>)
-// //             }
-// //             return (<li> ✅ </li>)
-// //         }
-
-// //         function StudentDetail() {
-
-// //             return (
-// //                 <div>
-// //                     <ul>CodeWars:
-// //                         <li>Current Total: {students.codewars.current.total} </li>
-// //                         <li>Last Week: {students.codewars.current.lastWeek}</li>
-// //                         <li>Goal: {students.codewars.goal.total}</li>
-// //                         <li>Percentage of Goal Achieved: {percentage} % </li>
-// //                     </ul>
-// //                     <ul>Scores:
-// //                         <li>{(students.cohort.scores.assignments) * 100} %</li>
-// //                         <li>{(students.cohort.scores.projects) * 100} %</li>
-// //                         <li>{(students.cohort.scores.assessments) * 100} %</li>
-// //                     </ul>
-// //                     <ul>Certifications:
-// //                         <li>Resume:{symbolForCert(students.certifications.resume)}</li>
-// //                         <li>LinkedIn: {symbolForCert(students.certifications.linkedin)}</li>
-// //                         <li>Github: {symbolForCert(students.certifications.github)}</li>
-// //                         <li>Mock Interview: {symbolForCert(students.certifications.mockInterview)}</li>
-// //                     </ul>
-// //                 </div>
-// //             )
-// //         }
-
-// //         //ternary (use semi colon for multiple conditions)or factor ;
-// //         function ShowMoreButton() {
-// //             if (isButtonPressed === false) {
-// //                 setExtraInfo(null)
-// //                 setChangeButtonText("Show More")
-// //             } else if (isButtonPressed === true) {
-// //                 setExtraInfo(StudentDetail())
-// //                 setChangeButtonText("Show Less")
-// //             }
-// //         }
-
-// //         return (
-// //             <div key={students.id} id={students.id} className="students">
-// //                 <img src={students.profilePhoto} width="90px" height="90px"></img>
-// //                 <h2>{students.names.preferredName} {students.names.middleName} {students.names.surname}</h2>
-// //                 <h3>{students.username}</h3>
-// //                 <h3><span>Birthday: </span>{students.dob}</h3>
-
-// //                 <OnTrackStatus
-// //                 className="onTrackStatus"
-// //                 data={data}
-// //                 students={students} />
-
-// //                 <div className="button">
-
-// //                     <button key={students.id} id={students.id} onClick={(e) => {
-
-// //                         setIsButtonPressed(!isButtonPressed)
-// //                         ShowMoreButton()
-
-// //                     }}>{changeButtonText}</button>
-
-// //                 </div>
-
-// //                 <div className='extraInfo'> {extraInfo} </div>
-
-// //             </div>
-// //         )
-// // })};
-
-//  return(
-// <>
-// <div className="cohortList">
-
-// <ul>Choose a Class By Start Date
-
-// <li>All Students</li>
-
-// <li onClick={()=>{
-//  console.log(selectedCohort.filter((semester)=>semester.cohort.cohortCode === season))
-// }}
-
-// >Winter 2026</li>
-
-// <li>Fall 2026</li>
-
-// <li>Summer 2026</li>
-
-// <li>Spring 2026</li>
-
-// <li>Winter 2025</li>
-
-// <li>Fall 2025</li>
-
-// <li>Summer 2025</li>
-
-// <li>Spring 2025</li>
-
-// </ul>
-
-// </div>
-// </>
-
-// )
-
-// }
-
-// export default CohortList
--- src/Components/StudentCard.jsx
+++ src/Components/StudentCard.jsx
@@ -1,46 +1,46 @@
 import OnTrackStatus from "./OnTrackStatus.jsx";
 import { useState } from "react";
+import "../Components/StudentCard.css"
 
-function StudentCard({students}){
+function StudentCard({ students }) {
 
     const [changeButtonText, setChangeButtonText] = useState("Show More")
     const [isButtonPressed, setIsButtonPressed] = useState(true)
     const [extraInfo, setExtraInfo] = useState(null)
-    
+
     function StudentDetail() {
-                    
+
         let percentage = Math.trunc((students.codewars.current.total / students.codewars.goal.total) * 100)
-        
-        
-                            function symbolForCert(cert) {
-                                if (cert === false) {
-                                    return <> ❌ </>
-                                }
-                                return <> ✅ </>
-                            }
-        
-                            return (
-                                <div key={students.id} className="studentDetail">
-                                    <ul>CodeWars:
-                                        <li>Current Total: {students.codewars.current.total} </li>
-                                        <li>Last Week: {students.codewars.current.lastWeek}</li>
-                                        <li>Goal: {students.codewars.goal.total}</li>
-                                        <li>Percentage of Goal Achieved: {percentage} % </li>
-                                    </ul>
-                                    <ul>Scores:
-                                        <li>{(students.cohort.scores.assignments) * 100} %</li>
-                                        <li>{(students.cohort.scores.projects) * 100} %</li>
-                                        <li>{(students.cohort.scores.assessments) * 100} %</li>
-                                    </ul>
-                                    <ul>Certifications:
-                                        <li>Resume:{symbolForCert(students.certifications.resume)}</li>
-                                        <li>LinkedIn: {symbolForCert(students.certifications.linkedin)}</li>
-                                        <li>Github: {symbolForCert(students.certifications.github)}</li>
-                                        <li>Mock Interview: {symbolForCert(students.certifications.mockInterview)}</li>
-                                    </ul>
-                                </div>
-                            )
-                        }
+
+        function symbolForCert(cert) {
+            if (cert === false) {
+                return <> ❌ </>
+            }
+            return <> ✅ </>
+        }
+
+        return (
+            <div key={students.id} className="studentDetail">
+                <ul>CodeWars:
+                    <li>Current Total: {students.codewars.current.total} </li>
+                    <li>Last Week: {students.codewars.current.lastWeek}</li>
+                    <li>Goal: {students.codewars.goal.total}</li>
+                    <li>Percentage of Goal Achieved: {percentage} % </li>
+                </ul>
+                <ul>Scores:
+                    <li>{(students.cohort.scores.assignments) * 100} %</li>
+                    <li>{(students.cohort.scores.projects) * 100} %</li>
+                    <li>{(students.cohort.scores.assessments) * 100} %</li>
+                </ul>
+                <ul>Certifications:
+                    <li>Resume:{symbolForCert(students.certifications.resume)}</li>
+                    <li>LinkedIn: {symbolForCert(students.certifications.linkedin)}</li>
+                    <li>Github: {symbolForCert(students.certifications.github)}</li>
+                    <li>Mock Interview: {symbolForCert(students.certifications.mockInterview)}</li>
+                </ul>
+            </div>
+        )
+    }
 
     function ShowMoreButton() {
         if (isButtonPressed === false) {
@@ -51,35 +51,35 @@ function StudentCard({students}){
             setChangeButtonText("Show Less")
         }
     }
-    
-return(
 
-<div key={students.id} id={students.id} className="students">
+    return (
 
-<img src={students.profilePhoto} width="90px" height="90px"></img>
-<h2>{students.names.preferredName} {students.names.middleName} {students.names.surname}</h2>
-<h3>{students.username}</h3>
-<h3><span>Birthday: </span>{students.dob}</h3>
+        <div key={students.id} id={students.id} className="students">
 
-<OnTrackStatus 
-className="onTrackStatus"
-students={students} />
+            <img src={students.profilePhoto} width="90px" height="90px"></img>
+            <h2>{students.names.preferredName} {students.names.middleName} {students.names.surname}</h2>
+            <h3>{students.username}</h3>
+            <h3><span>Birthday: </span>{students.dob}</h3>
 
-<div className="button">
+            <OnTrackStatus
+                className="onTrackStatus"
+                students={students} />
 
-    <button key={students.id} id={students.id} onClick={(e) => {
+            <div className="button">
 
-        setIsButtonPressed(!isButtonPressed)
-        ShowMoreButton()
+                <button key={students.id} id={students.id} onClick={(e) => {
 
-    }}>{changeButtonText}</button>
+                    setIsButtonPressed(!isButtonPressed)
+                    ShowMoreButton()
 
-</div>
+                }}>{changeButtonText}</button>
 
-<div className='extraInfo'> {extraInfo} </div>
+            </div>
 
-</div>
-)
+            <div className='extraInfo'> {extraInfo} </div>
+
+        </div>
+    )
 }
 
 export default StudentCard
